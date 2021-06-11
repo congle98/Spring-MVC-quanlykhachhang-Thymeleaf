43c9a3cc65017f2b98a041ded7ccfc85ceb62420
congle98 <79907265+congle98@users.noreply.github.com>
2021-06-11 17:55:35 +0700

    quan ly khach khang thymeleaf

---
+++ src/main/java/com/app/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.app.service;
+
+import com.app.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
